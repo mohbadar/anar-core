345850e0bbb629a52260323c9d7ec0b91bb8adf0
mohbadar <mohammadbadarhashimi@gmail.com>
2020-01-11 12:17:21 +0430

    init

---
+++ src/main/java/af/gov/anar/lang/anarlang/AnarLangApplication.java
@@ -0,0 +1,13 @@
+package af.gov.anar.lang.anarlang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnarLangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnarLangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/af/gov/anar/lang/anarlang/AnarLangApplicationTests.java
@@ -0,0 +1,13 @@
+package af.gov.anar.lang.anarlang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnarLangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
